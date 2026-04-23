fc0d79daf13f12b9cd2c4c22d405b1bfce8934f3
Jess Nunes <eng.diias@gmail.com>
2026-04-23 06:57:35 -0300

    Primeiro commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Projeto de classificacao de clientes da base marktplace Olist',
+    author='jessica nunes',
+    license='MIT',
+)
